be566246c936b2cbd465c328e37939417d3d5d9f
BriyanaChataigne <briyana.chataigne@techhigh.us>
2015-08-07 21:01:55 -0400

    added do.java

---
+++ do.java
@@ -0,0 +1,13 @@
+ public class apples {
+		public static void main (String args[]){
+			int counter = 3;
+			
+			do{
+			    System.out.println(counter);	
+			    counter++;
+			}while(counter <=10);
+		
+		
+			
+		}
+	}
